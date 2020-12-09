b94dbb106d51350d99aed54ef8ca41d933129c7e
EmilStalvinge <emilstalvinge@gmail.com>
2020-12-09 15:56:38 +0100

    print

--- serialtest.py
+++ serialtest.py
@@ -5,4 +5,6 @@ ser = serial.Serial('/dev/ttyUSB0',9600)
 while True:
 	read_serial=ser.readline()
 	mylist = read_serial.split()
-	print(mylist[0]) #read_serial
+	#print(mylist[0]) #read_serial
+	print(f'node id: {mylist[0]}.'})
+	print(f'parking 1: {mylist[1]}.'})
