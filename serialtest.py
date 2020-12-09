029160315d4c52906f82deaed56b9ede61cb70ac
EmilStalvinge <emilstalvinge@gmail.com>
2020-12-09 15:49:07 +0100

    update

--- serialtest.py
+++ serialtest.py
@@ -1,6 +1,8 @@
 import serial
 
-ser = serial.Serial('/dev/ttyACM0',9600)
+#ser = serial.Serial('/dev/ttyACM0',9600)
+ser = serial.Serial('/dev/ttyUSB0',9600)
 while True:
 	read_serial=ser.readline()
-	print read_serial
+	mylist = read_serial.split()
+	print(mylist[0]) #read_serial
